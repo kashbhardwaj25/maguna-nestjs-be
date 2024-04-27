1486e957aa715f7fa9a610d8f8379c967cf61b1a
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-04-27 19:02:39 +0530

    Postgres and Sequelize Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { User } from './users/user.model';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'maguna_db',
+      models: [User], // Include all models here
+      autoLoadModels: true, // Automatically load models
+      synchronize: true, // Synchronize models with the database (not recommended for production)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
