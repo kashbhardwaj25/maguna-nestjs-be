cd74a5831cbf3950a34c9b089e4df030e1a830ce
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-16 15:24:27 +0530

    UUID based user ids implemented.

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,7 +1,20 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import {
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  id: string;
+
   @Column
   name: string;
 
