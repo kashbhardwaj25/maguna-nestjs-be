9af90aab91911d30c6fcaedbbd438e1feb654c06
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-17 19:30:21 +0530

    Email service refactored.

--- src/services/sendgrid.service.ts
+++ src/services/sendgrid.service.ts
@@ -1,13 +1,12 @@
 import * as sgMail from '@sendgrid/mail';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 import { config } from 'src/config';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly configService: ConfigService) {
-    sgMail.setApiKey(this.configService.get<string>(config.sendGridApiKey));
+  constructor() {
+    sgMail.setApiKey(config.sendGridApiKey);
   }
 
   async sendEmail(to: string, subject: string, html: string) {
@@ -18,13 +17,13 @@ export class EmailService {
       html,
     };
 
-    try {
-      await sgMail.send(msg);
-    } catch (error) {
-      console.error(`Error sending email to ${to}: `, error);
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
