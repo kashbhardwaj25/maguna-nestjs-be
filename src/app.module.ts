089a490aef6a24adbc0431c18d2a98a23c9e24b3
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-18 13:01:52 +0530

    Modules added in a separate folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
-import { GraphQLModule } from './graphql.module';
-import { ScheduleModule } from '@nestjs/schedule';
+import { GraphQLModule } from './modules/graphql.module';
 import { UsersModule } from './users/users.module';
-import { SequelizeModule } from './sequelize.module';
+import { SequelizeModule } from './modules/sequelize.module';
+import { CronModule } from './modules/cron.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ScheduleModule.forRoot(),
+    CronModule,
     GraphQLModule,
     SequelizeModule,
     UsersModule,
