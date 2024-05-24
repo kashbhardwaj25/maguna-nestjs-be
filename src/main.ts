9883e66402511c641828ee55a96724e3b34ea3c6
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-24 18:06:18 +0530

    Cookie parser added. isEmailVerified in user schema.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  });
 }
 bootstrap();
